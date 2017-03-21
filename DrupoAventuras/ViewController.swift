761b8c3d76232c525c91e5c8fb25b184bfb6390a
Aldo Roldan Hernandez Rea <aldo@MacBook-Pro-de-Aldo.local>
2017-03-20 18:20:26 -0600

    Initial Commit

---
+++ DrupoAventuras/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DrupoAventuras
+//
+//  Created by Aldo Roldan Hernandez Rea on 20/03/17.
+//  Copyright © 2017 Aldo Roldan Hernandez Rea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
